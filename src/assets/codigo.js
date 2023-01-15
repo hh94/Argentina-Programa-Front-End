745ad890c90b75319fdecbe324062acf9056f1cb
hh94 <hirohiga.hh@gmail.com>
2023-01-15 10:16:14 -0300

    porafolio

--- src/assets/codigo.js
+++ src/assets/codigo.js
@@ -1,13 +1,15 @@
-//----------modal funcion--------------------------------------------------------------------------------->
+    //----------modal funcion--------------------------------------------------------------------------------->
 
     // Get the modal ingresar
     var modal = document.getElementById('id01');
     // Get the modal registrar
     var modal2 = document.getElementById('id02');
     
+    
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
   }
+}
